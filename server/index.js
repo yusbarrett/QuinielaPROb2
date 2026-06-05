59b41a3f50fc5cc5a4b5e158c91be577684725fd
yusbarrett <yusbarrett@gmail.com>
2026-06-04 23:23:00 -0600

    Serve client build static assets unconditionally for Azure deployment

--- server/index.js
+++ server/index.js
@@ -101,12 +101,11 @@ app.get('/api/users', (req, res) => {
   res.json(db.users);
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-  });
-}
+const clientBuildPath = path.join(__dirname, '..', 'client', 'build');
+app.use(express.static(clientBuildPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
